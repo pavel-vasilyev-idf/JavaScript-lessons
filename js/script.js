6c9318781d820b27e1e55a98cebdad3ed40d600a
pavel.vasilyev <pavel.vasilyev@moneyman.ru>
2021-04-01 12:41:09 +0300

    feat: writed first script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert('Hi, Sasha. It is my first alert!))Maby');
+
+console.log('It is my first log!))Maby');
+console.log('There are no errors in the console');
