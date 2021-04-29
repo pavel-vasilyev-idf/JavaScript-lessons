29b8b27cee617009cc488bf3c258ec8d533e33f6
pavel.vasilyev <pavel.vasilyev@moneyman.ru>
2021-04-29 11:24:27 +0300

    feat: repeat over video

--- js/script.js
+++ js/script.js
@@ -420,5 +420,55 @@ window.addEventListener('DOMContentLoaded', () => {
 	})
 
 
+	//calculator
+	const calc = (price = 100) => {
+		const calcBlock = document.querySelector('.calc-block');
+		const calcType = document.querySelector('.calc-type');
+		const calcDay = document.querySelector('.calc-day');
+		const calcSquare = document.querySelector('.calc-square');
+		const calcCount = document.querySelector('.calc-count');
+		const totalValue = document.getElementById('total');
+
+		const countSum = () => {
+			let total = 0;
+			let countValue = 1;
+			let dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value;
+			const squareValue = +calcSquare.value;
+
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			};
+
+			if (calcDay.value && calcDay.value < 5) {
+				dayValue *=  2;
+			} else if (calcDay.value && calcDay.value < 10) {
+				dayValue *=  1.5;
+			};
+
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			};
+
+			totalValue.textContent = total;
+
+		};
+
+		calcBlock.addEventListener('change', (event) => {
+			const target = event.target;
+
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			};
+		}); 
+
+
+
+
+
+	}
+
+	calc(100);
+
 });
  
